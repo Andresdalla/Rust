26f9bfa8df6a9511277d266b143450489120f88a
Andres Dalla Rizza <andresdallarizza1081@gmail.com>
2024-08-24 18:37:03 -0300

    first commit

---
+++ hello/target/debug/deps/hello-0d4d531c494cce0a.d
@@ -0,0 +1,5 @@
+C:\Users\andre\rust\hello\target\debug\deps\libhello-0d4d531c494cce0a.rmeta: src/main.rs
+
+C:\Users\andre\rust\hello\target\debug\deps\hello-0d4d531c494cce0a.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/deps/hello-1bea10c0047b5505.d
@@ -0,0 +1,5 @@
+C:\Users\andre\rust\hello\target\debug\deps\libhello-1bea10c0047b5505.rmeta: src/main.rs
+
+C:\Users\andre\rust\hello\target\debug\deps\hello-1bea10c0047b5505.d: src/main.rs
+
+src/main.rs:
